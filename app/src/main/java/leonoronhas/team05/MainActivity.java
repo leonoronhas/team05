09e6758187c67e30d23cec16ea8e22bd3ea2ad18
leonoronsantos <leosud099@gmail.com>
2019-10-16 20:35:45 -0600

    Initial commit

---
+++ app/src/main/java/leonoronhas/team05/MainActivity.java
@@ -0,0 +1,27 @@
+package leonoronhas.team05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "leonoronhas.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Calls when the user presses the send button
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
